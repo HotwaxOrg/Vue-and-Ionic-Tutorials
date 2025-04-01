abcdeb1814b4d6d1365be8ef97aa3e4c4df83d05
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-01 20:16:06 +0530

    Form Handling in vue directives

--- VueTutorials/src/App.vue
+++ VueTutorials/src/App.vue
@@ -1,4 +1,4 @@
-<script>  
+<script setup>  
   import { ref } from 'vue';
   // Option api
   // export default{
@@ -22,28 +22,29 @@
   // }
 
   // Composition api
-  export default{
-    setup(){
-      const name = ref("Viresh Shah");
-      const status = ref("active");
-      const tasks = ref(["Task1", "Task2", "Task3"]);
+  const name = ref("Viresh Shah");
+  const status = ref("active");
+  const tasks = ref(["Task1", "Task2", "Task3"]);
+  const newTask = ref('');
 
-      const toggleStatus = () => {
-        if (status.value === "active"){
-          status.value = "inactive";
-        } else{
-          status.value = "active";
-        }
-      };
-
-      return {
-        name, 
-        status,
-        tasks,
-        toggleStatus,
-      }
+  const toggleStatus = () => {
+    if (status.value === "active"){
+      status.value = "inactive";
+    } else{
+      status.value = "active";
     }
   };
+
+  const addTasks = () => {
+    if (newTask.value !== ''){
+      tasks.value.push(newTask.value);
+      newTask.value = '';
+    }
+  };
+
+  const deleteTasks = (index) => {
+    tasks.value.splice(index, 1);
+  }
 </script>
 
 <template>
@@ -64,7 +65,12 @@
   <!-- Looping directives -->
    <h3>Tasks:</h3>
    <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTasks(index)">x</button>
+    </li>
    </ul>
 
    <!-- Bind directives -->
@@ -73,5 +79,13 @@
 
    <!-- Events and Methods -->
     <button @click="toggleStatus">Change Status!</button>
+
+    <!-- Form Handling -->
+     <h2>Add a new Task</h2>
+     <form @submit.prevent="addTasks">
+        <label for="newTask">Add Task: &nbsp;</label>
+        <input type="text" name="newTask" id="newTask" v-model="newTask"> &nbsp;
+        <button type="submit">Add Task!</button>
+     </form>
   </template>
 
