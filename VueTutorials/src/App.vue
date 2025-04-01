7d7073fbfb0d3df7424397d4100cd333d58e61da
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-01 19:20:44 +0530

    Practiced v-if, v-else-if directives

--- VueTutorials/src/App.vue
+++ VueTutorials/src/App.vue
@@ -1,5 +1,24 @@
-<template>
-  <h1>Setup successful</h1>  
-</template>
+<script>
+  export default{
+    data(){
+      return {
+        name: "Viresh Shah",
+        status: "active",
+      };
+    },
+  }
+</script>
 
+<template>
+  <h1>{{ name }}</h1> 
+  <p v-if="status === 'active'">
+    "User is active!"
+  </p> 
+  <p v-else-if="status === 'pending'">
+    "Activation Pending"
+  </p>
+  <p v-else>
+    "User is inactive!"
+  </p>
+</template>
 
