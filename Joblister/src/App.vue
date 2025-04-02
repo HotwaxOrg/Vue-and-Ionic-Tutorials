130b5e6d3b08a16a5b4bd7dbba330d26eca34d8b
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-02 20:42:32 +0530

    Created a card component and homecard component

--- Joblister/src/App.vue
+++ Joblister/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
   import Navbar from '@/components/Navbar.vue'
   import Hero from '@/components/Hero.vue'
+  import HomeCard from '@/components/HomeCards.vue'
 </script>
 <template>
   <Navbar />
   <Hero title="Test Props" subtitle="Test Prop Subtitel"/>
+  <HomeCard />
 </template>
 
