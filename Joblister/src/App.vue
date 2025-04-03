7695c25f25ef8dc93b07a64caca39b6f763486b0
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-03 10:40:07 +0530

    Created components to list jobs and completed the home page setup

--- Joblister/src/App.vue
+++ Joblister/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-  import Navbar from '@/components/Navbar.vue'
-  import Hero from '@/components/Hero.vue'
-  import HomeCard from '@/components/HomeCards.vue'
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/Hero.vue';
+  import HomeCard from '@/components/HomeCards.vue';
+  import Joblisting from '@/components/Joblisting.vue';
 </script>
 <template>
   <Navbar />
   <Hero title="Test Props" subtitle="Test Prop Subtitel"/>
   <HomeCard />
+  <Joblisting :limit="3" :showButton="true"/>
 </template>
 
