00cb6b6691c14ea6885f6e59440db7ed1df823f1
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-04 10:03:07 +0530

    Learned about router in vue and setuped the home-page router

---
+++ Joblister/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Hero from '@/components/Hero.vue';
+  import HomeCard from '@/components/HomeCards.vue';
+  import Joblisting from '@/components/Joblisting.vue';
+</script>
+<template>
+  <Hero title="Test Props" subtitle="Test Prop Subtitel"/>
+  <HomeCard />
+  <Joblisting :limit="3" :showButton="true"/>
+</template>
+
