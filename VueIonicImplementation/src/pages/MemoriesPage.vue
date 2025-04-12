77cfcb3d6944e1d8c08a165a848e62694407a21b
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-04-12 11:24:55 +0530

    Created a MemoriesPage.vue to display memories

---
+++ VueIonicImplementation/src/pages/MemoriesPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All Memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>Viresh</ion-item>
+                <ion-item>Luffy</ion-item>
+                <ion-item>Itachi</ion-item>
+                <ion-item>Zoro</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+    import { IonPage, IonHeader, IonTitle, IonContent, IonToolbar, IonList, IonItem } from '@ionic/vue';
+</script>
